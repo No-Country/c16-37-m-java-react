aeef38e07dcf50a9ab45938f2ac33b5280e5718e
EmiLongo <140227770+EmiLongo@users.noreply.github.com>
2024-02-15 10:45:07 -0300

    se arma el main con las categorias

--- frontend/src/components/bodyComponent.jsx
+++ frontend/src/components/bodyComponent.jsx
@@ -1,8 +1,39 @@
 import '../assets/styles/bodyComponent.css'
+import bgZara1 from '../assets/img/bodyComponent/bg-zara-dresses-1.jpg'
+import bgZara2 from '../assets/img/bodyComponent/bg-zara-dresses-2.jpg'
+import bgZara3 from '../assets/img/bodyComponent/bg-zara-dresses-3.jpg'
+import filterCatWomen from '../assets/img/bodyComponent/category-women.jpg'
+import filterCatMen   from '../assets/img/bodyComponent/category-men.jpg'
+import filterCatKids from '../assets/img/bodyComponent/category-kids.webp'
 export const BodyComponent = () => {
   return (
     <>
     <h1 className="text-3xl font-bold underline">No Country - c16-31-m-java-react</h1>
+    <div className='bg-container'>
+    <div className='bg-zara-2'><img src={bgZara2} alt="" /></div>
+    <div className='bg-zara-1'><img src={bgZara1} alt="" /></div>
+    <div className='bg-zara-3'><img src={bgZara3} alt="" /></div>
+    </div>
+    <div className='category-container'>
+      <div className='category-card'>
+        <img src={filterCatWomen} alt="foto de mujer" />
+        <div>
+          <h3>MUJER</h3> 
+        </div>
+      </div>
+      <div className='category-card'>
+        <img src={filterCatMen} alt="foto de hombre" />
+        <div>
+          <h3>HOMBRE</h3>
+        </div>
+      </div>
+      <div className='category-card'>
+        <img src={filterCatKids} alt="foto de niños" />
+        <div>
+          <h3>NIÑOS</h3>
+        </div>
+      </div>
+    </div>
     </>
     )
 }
--- frontend/src/components/headerComponent.jsx
+++ frontend/src/components/headerComponent.jsx
@@ -6,37 +6,42 @@ import iconShoppingBag from "../assets/img/icons/shopping-bag2.svg";
 export const HeaderComponent = () => {
   return (
     <>
-      <div className ="header-container">
-        <img className="nav-logo" src={imgLogo} alt="" width="300"/>
-        <div className ="nav-icon-container">
-        <img className="nav-icon" src={iconMenuHamburger} alt="" width="32" />
-        <div className="nav-icon-right flex flex-row flex-wrap items-center">
-            <form className="nav-search group relative">
-              <svg
-                width="20"
-                height="20"
-                fill="currentColor"
-                className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-gray-500"
-                aria-hidden="true"
-                >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  />
-              </svg>
-              <input
-                className="w-80 focus:ring-2 focus:ring-gray-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"
-                type="text"
-                aria-label="Filter projects"
-                placeholder="Tipo de prenda..."
-                />
-            </form>
-        <img className="nav-icon mx-4" src={iconUser} alt="" width="32" />
-        <img className="nav-icon mx-4" src={iconShoppingBag} alt="" width="32" />
-        </div>
+      <div className ="header-container header-container-initial">
+        <div className="nav-container ">
+            <img className="nav-logo" src={imgLogo} alt="" width="300"/>
+            <div className ="nav-icon-container">
+            <img className="nav-icon" src={iconMenuHamburger} alt="" width="32" />
+            <div className="nav-icon-right flex flex-row flex-wrap items-center">
+                <form className="nav-search group relative">
+                  <svg
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-gray-500"
+                    aria-hidden="true"
+                    >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      />
+                  </svg>
+                  <input
+                    className="w-80 focus:ring-2 focus:ring-gray-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"
+                    type="text"
+                    aria-label="Filter projects"
+                    placeholder="Tipo de prenda..."
+                    />
+                </form>
+            <img className="nav-icon mx-4" src={iconUser} alt="" width="32" />
+            <img className="nav-icon mx-4" src={iconShoppingBag} alt="" width="32" />
+            </div>
 
+            </div>
+            
         </div>
+        <div className="nav-call-action">NUEVA COLECCIÓN
+        </div>    
       </div>
     </>
   );
