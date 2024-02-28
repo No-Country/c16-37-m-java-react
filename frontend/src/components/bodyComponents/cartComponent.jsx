34786db9cd8e41786b961b85dd2428ea53d1222e
EmiLongo <140227770+EmiLongo@users.noreply.github.com>
2024-02-28 19:41:02 -0300

    Se le da formato al paso 2 del carrito

--- frontend/src/components/bodyComponents/cartComponent.jsx
+++ frontend/src/components/bodyComponents/cartComponent.jsx
@@ -7,8 +7,8 @@ const CartComponent = () => {
         <div className='cart-steps max-w-80 mx-auto mb-8'>
             <div className="after:mt-4 after:block after:h-1 after:w-full after:rounded-lg after:bg-gray-200">
                 <ol className="grid grid-cols-3 text-sm font-medium text-gray-500">
-                <li className="relative flex justify-start text-blue-600">
-                    <span className="absolute -bottom-[1.75rem] start-0 rounded-full bg-blue-600 text-white">
+                <li className="relative flex justify-start text-gray-900">
+                    <span className="absolute -bottom-[1.75rem] start-0 rounded-full bg-gray-900 text-white">
                     <svg
                         className="h-5 w-5"
                         xmlns="http://www.w3.org/2000/svg"
@@ -41,9 +41,9 @@ const CartComponent = () => {
                     </svg>
                 </li>
 
-                <li className="relative flex justify-center text-blue-600">
+                <li className="relative flex justify-center text-gray-900">
                     <span
-                    className="absolute -bottom-[1.75rem] left-1/2 -translate-x-1/2 rounded-full bg-blue-600 text-white"
+                    className="absolute -bottom-[1.75rem] left-1/2 -translate-x-1/2 rounded-full bg-gray-900 text-white"
                     >
                     <svg
                         className="h-5 w-5"
