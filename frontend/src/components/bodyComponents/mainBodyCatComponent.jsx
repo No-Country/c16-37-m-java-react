a16e654a032b482a7fcdbde57802628d41a75cf0
Andres0210 <ing_amilkar@hotmail.com>
2024-02-29 09:47:14 -0500

    cambios de redux en el front

--- frontend/src/components/bodyComponents/mainBodyCatComponent.jsx
+++ frontend/src/components/bodyComponents/mainBodyCatComponent.jsx
@@ -1,42 +1,48 @@
-
-import filterCatWomen from '../../assets/img/bodyComponent/category-women.jpg'
-import filterCatMen   from '../../assets/img/bodyComponent/category-men.jpg'
-import filterCatKids from '../../assets/img/bodyComponent/category-kids.jpg'
-import { Link } from 'react-router-dom'
-
+import filterCatWomen from "../../assets/img/bodyComponent/category-women.jpg";
+import filterCatMen from "../../assets/img/bodyComponent/category-men.jpg";
+import filterCatKids from "../../assets/img/bodyComponent/category-kids.jpg";
+import { Link } from "react-router-dom";
+import { products } from "../../assets/productosPrueba";
+import axios from "axios";
 
 const MainBodyCatComponent = () => {
+  const loadProducts = () => {
+    products.forEach(async (element) => {
+      await axios.post("http://localhost:8081/producto", element);
+    });
+  };
 
   return (
-    <div className='category-container'>
-        <div 
-        className='category-card'>
-            <Link to={'/products'}>
-                <img src={filterCatWomen} alt="foto de mujer" />
-                <div>
-                    <h3>MUJER</h3> 
-                </div>
-            </ Link>
-        </div>
+    <div className="category-container">
+      <button onClick={loadProducts}>Cargar products</button>
+      <div className="category-card">
+        <Link to={"/products"}>
+          <img src={filterCatWomen} alt="foto de mujer" />
+          <div>
+            <h3>MUJER</h3>
+          </div>
+        </Link>
+      </div>
+      <div className="category-card">
+        s
+        <Link to={"/products"}>
+          <img src={filterCatMen} alt="foto de hombre" />
+          <div>
+            <h3>HOMBRE</h3>
+          </div>
+        </Link>
+      </div>
 
-        <div className='category-card'>
-            <Link to={'/products'}>
-                <img src={filterCatMen} alt="foto de hombre" />
-                <div>
-                    <h3>HOMBRE</h3>
-                </div>
-            </Link>
-        </div>
+      <div className="category-card">
+        <Link to={"/products"}>
+          <img src={filterCatKids} alt="foto de niños" />
+          <div>
+            <h3>NIÑOS</h3>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-        <div className='category-card'>
-            <Link to={'/products'}>
-                <img src={filterCatKids} alt="foto de niños" />
-                <div>
-                    <h3>NIÑOS</h3>
-                </div>
-            </Link>
-        </div>
-    </div>  )
-}
-
-export default MainBodyCatComponent
+export default MainBodyCatComponent;
--- frontend/src/redux/reducer.js
+++ frontend/src/redux/reducer.js
@@ -1,12 +1,19 @@
-import { GET_PRODUCTS } from "./actions";
+import { GET_PRODUCTS, GET_PRODUCTS_SEARCH } from "./actions";
 
 const initialState = {
   allProducts: [],
   productsToShow: [],
+  
 };
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
+      return {
+        ...state,
+        allProducts: action.payload,
+        productsToShow: action.payload,
+      };
+    case GET_PRODUCTS_SEARCH:
       return {
         ...state,
         allProducts: action.payload,
